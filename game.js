a780310d50b47c014c21591ad989f0b5ad85b14d
Raresconstantinescu2005 <rares.a.constantinescu@gmail.com>
2020-11-14 11:05:02 +0200

    MY first program

--- game.js
+++ game.js
@@ -3,21 +3,21 @@
 
 import Phaser from 'phaser'
 
-class Game extends Phasere.Scene{
+class Game extends Phasere.Scene {
 
 
-preload(){
-    this.preload.image('mage', 'D:/coderdojo/platform-game2-Raresconstantinescu2005/asset/Mage/mage.png');
-}
+    preload() {
+        this.preload.image('mage', 'D:/coderdojo/platform-game2-Raresconstantinescu2005/asset/Mage/mage.png');
+    }
 
 
 
-create() {
-    this hero = this.psyhic.add.
-    this.physics.add.sprite(400, 300, 'mage');  
+    create() {
+        this hero = this.psyhic.add.sprite(400; 300; 'mage')
+        this.hero.body.setColideworldBounds(true)
 
 
-}
+    }
 
 }
 export default Game
