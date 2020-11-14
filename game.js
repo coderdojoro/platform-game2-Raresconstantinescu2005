a16dd4970810353b8717e8836b9cbb5605d48b1e
Raresconstantinescu2005 <rares.a.constantinescu@gmail.com>
2020-11-14 10:54:27 +0200

    My first sprite

---
+++ game.js
@@ -0,0 +1,23 @@
+
+
+
+import Phaser from 'phaser'
+
+class Game extends Phasere.Scene{
+
+
+preload(){
+    this.preload.image('mage', 'D:/coderdojo/platform-game2-Raresconstantinescu2005/asset/Mage/mage.png');
+}
+
+
+
+create() {
+    this hero = this.psyhic.add.
+    this.physics.add.sprite(400, 300, 'mage');  
+
+
+}
+
+}
+export default Game
